b1110ba8172793c7f92549cd01f38388fea5d172
nlabrazi <na.labrazi@gmail.com>
2025-06-13 03:07:16 +0200

    fix(chore): multiple change regarding vercel

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,14 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(
     private prisma: PrismaService,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   async validateUser(email: string, password: string) {
     const user = await this.prisma.user.findUnique({ where: { email } });
--- api/src/main.ts
+++ api/src/main.ts
@@ -2,32 +2,47 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bodyParser: false, // désactive le parser JSON global
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bodyParser: false,
   });
 
-  // --- Middleware pour le webhook Stripe ---
-  app.use('/webhooks/stripe', express.raw({ type: (_req) => true }));
-
-  // --- JSON pour le reste de l'API ---
+  // --- CONFIG HERE ---
+  app.use('/webhooks/stripe', express.raw({ type: () => true }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
+  // global prefix for all API routes
+  app.setGlobalPrefix('api');
+
   app.enableCors({
     origin: [
-      'http://localhost:4000', // Dev UI
-      'https://staging.argandici.com', // Staging
-      'https://www.argandici.com' // Production
+      'http://localhost:4000',
+      'https://argandici.com',
+      'https://www.argandici.com',
+      // URL preview for staging if i can
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // Validation des DTO
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  // --- NEW LOGIC ---
+  // Vercel define variable env 'VERCEL'.
+  // If doesn't exist we use local Docker.
+  if (!process.env.VERCEL) {
+    // If local, start server as usual.
+    await app.listen(3000, '0.0.0.0');
+    console.log(`🚀 API running locally on: ${await app.getUrl()}`);
+  }
+
+  // For Vercel, init app & return 'handler'.
+  await app.init();
+  return app.getHttpAdapter().getInstance();
 }
-bootstrap();
+
+// Export result & fonction for Vercel.
+export default bootstrap();
